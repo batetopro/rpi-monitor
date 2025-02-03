f7f943500b3b8c0c8a56e0a3a78480546d83a689
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2025-02-03 18:46:55 +0200

    Commands for fetching host and usage information

--- app/commands.py
+++ app/commands.py
@@ -1,27 +1,48 @@
+import datetime
+import json
+
+
 from app import app
 from app.info.host import HostInfo
 
 
-@app.cli.command("stats")
-def get_stats():
+@app.cli.command("host")
+def get_host():
+    host_info = HostInfo()
+    platform_info = host_info.read_platform()
+
+    data = {
+        'hostname': host_info.hostname,
+        'model': host_info.model,
+        'os_name': platform_info['pretty_name'],
+        'system': platform_info['system'],
+        'machine': platform_info['machine'],
+        'processor': platform_info['processor'],
+        'platform': platform_info['platform'],
+        'up_since': datetime.datetime.strptime(
+            host_info.up_since,
+            '%Y-%m-%d %H:%M:%S'
+        ).timestamp(),
+        'max_cpu_frequency': host_info.cpu_max_frequency,
+        'total_ram': host_info.ram_total,
+        'number_of_cpus': host_info.number_of_cpus,
+    }
+
+    print(json.dumps(data))
+
+
+@app.cli.command("usage")
+def get_usage():
     host_info = HostInfo()
 
-    print('Hello from: ' + host_info.hostname)
-    print('Model: ' + host_info.model)
-    
-    print('CPU usage: {}'.format(host_info.cpu_usage_percent))
-    print('CPU frequency: {}'.format(host_info.cpu_frequency))
-    print('CPU max frequency: {}'.format(host_info.cpu_max_frequency))
-    print('CPU Temperature: {}'.format(host_info.cpu_temp))
-    
-    print('RAM total: {}'.format(host_info.ram_total))
-    print('RAM free: {}'.format(host_info.ram_free))
-    print('RAM used: {}'.format(host_info.ram_used))
-    print('RAM used percent: {}'.format(host_info.ram_used_percent))
-    
-    print('Up for: {}'.format(host_info.up_for))
-    print('Up since: {}'.format(host_info.up_since))
-    print('Current date: {}'.format(host_info.current_date))
+    data = {
+        'cpu_usage': host_info.cpu_usage_percent,
+        'cpu_frequency': host_info.cpu_frequency,
+        'cpu_temperature': host_info.cpu_temp,
+        'ram': host_info.ram_used,
+        'disk_space_available': host_info.disk_space_available,
+        'disk_space_used': host_info.disk_space_used,
+        'current_date': host_info.current_date,
+    }
 
-    print(host_info.read_platform())
-    
+    print(json.dumps(data))
