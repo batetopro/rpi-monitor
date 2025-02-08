30d6a858e677ae6d01c974b5c999b8160c99c131
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2025-02-08 20:15:03 +0200

    Add error logging and do not redeclare app

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash
@@ -11,14 +16,29 @@ app.config.from_object(Settings)
 auth = HTTPBasicAuth()
 
 
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/rpi-monitor.log', maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('RPi monitor startup')
+
+
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
-
-
-app = Flask(__name__)
 
 
 from app import routes, models, commands, sidebar
