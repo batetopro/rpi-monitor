a6a4c6d5df1e00fe0a7214845239909bfaeac554
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2025-02-18 07:25:34 +0200

    Refactor output to be collected from reports

--- app/commands.py
+++ app/commands.py
@@ -1,70 +1,20 @@
 import json
-import os
 import sys
 
 
 from app import app
-from app.info.platform_info import PlatformInfo
-from app.info.psutil_info import PsutilHostInfo
+from app.reports import reports
 
 
 @app.cli.command("chat")
 def chat():
     while 1:
         query = sys.stdin.readline()
-        if query == 'host':
-            host_info = PsutilHostInfo()
-            platform_info = PlatformInfo.read()
-
-            data = {
-                'hostname': host_info.hostname,
-                'model': host_info.model,
-                'os_name': platform_info['pretty_name'],
-                'system': platform_info['system'],
-                'machine': platform_info['machine'],
-                'processor': platform_info['processor'],
-                'platform': platform_info['platform'],
-                'up_since': host_info.up_since,
-                'max_cpu_frequency': host_info.cpu_max_frequency,
-                'net_interfaces': host_info.net_interfaces,
-                'total_ram': host_info.ram_total,
-                'number_of_cpus': host_info.number_of_cpus,
-            }
-
-        elif query == 'usage':
-            host_info = PsutilHostInfo()
-            current_date = host_info.current_date
-
-            for partition in host_info.disk_partitions:
-                dev = os.path.basename(partition['device'])
-                if dev in host_info.disk_io_counters:
-                    counters = host_info.disk_io_counters[dev]
-                    partition['io_read_bytes'] = counters.read_bytes
-                    partition['io_read_count'] = counters.read_count
-                    partition['io_read_time'] = counters.read_time
-                    partition['io_write_count'] = counters.write_count
-                    partition['io_write_bytes'] = counters.write_bytes
-                    partition['io_write_time'] = counters.write_time
-
-            data = {
-                'cpu_usage': host_info.cpu_usage_percent,
-                'cpu_frequency': host_info.cpu_frequency,
-                'cpu_temperature': host_info.cpu_temp,
-                'current_date': current_date,
-                'disk_io_read_bytes': host_info.disk_io_read_bytes,
-                'disk_io_write_bytes': host_info.disk_io_write_bytes,
-                'disk_space_available': host_info.disk_space_available,
-                'disk_space_used': host_info.disk_space_used,
-                'disk_space_total': host_info.disk_space_total,
-                'disk_partitions': host_info.disk_partitions,
-                'net_io_bytes_recv': host_info.net_io_bytes_recv,
-                'net_io_bytes_sent': host_info.net_io_bytes_sent,
-                'ram': host_info.ram_used,
-                'swap_used': host_info.swap_used,
-                'swap_total': host_info.swap_total,
-            }
-        elif query == 'quit':
+        if query == 'quit':
             break
+
+        if query in reports:
+            data = reports[query]()
         else:
             data = False
 
@@ -74,70 +24,17 @@ def chat():
 
 @app.cli.command("host")
 def get_host():
-    host_info = PsutilHostInfo()
-    platform_info = PlatformInfo.read()
-
-    data = {
-        'hostname': host_info.hostname,
-        'model': host_info.model,
-        'os_name': platform_info['pretty_name'],
-        'system': platform_info['system'],
-        'machine': platform_info['machine'],
-        'processor': platform_info['processor'],
-        'platform': platform_info['platform'],
-        'up_since': host_info.up_since,
-        'max_cpu_frequency': host_info.cpu_max_frequency,
-        'net_interfaces': host_info.net_interfaces,
-        'total_ram': host_info.ram_total,
-        'number_of_cpus': host_info.number_of_cpus,
-    }
-
+    data = reports['host']()
     print(json.dumps(data))
 
 
 @app.cli.command("net_interfaces")
 def get_net_interfaces():
-    host_info = PsutilHostInfo()
-
-    data = {
-        'net_interfaces': host_info.net_interfaces,
-    }
-
+    data = reports['net_interfaces']()
     print(json.dumps(data))
 
 
 @app.cli.command("usage")
 def get_usage():
-    host_info = PsutilHostInfo()
-    current_date = host_info.current_date
-
-    for partition in host_info.disk_partitions:
-        dev = os.path.basename(partition['device'])
-        if dev in host_info.disk_io_counters:
-            counters = host_info.disk_io_counters[dev]
-            partition['io_read_bytes'] = counters.read_bytes
-            partition['io_read_count'] = counters.read_count
-            partition['io_read_time'] = counters.read_time
-            partition['io_write_count'] = counters.write_count
-            partition['io_write_bytes'] = counters.write_bytes
-            partition['io_write_time'] = counters.write_time
-
-    data = {
-        'cpu_usage': host_info.cpu_usage_percent,
-        'cpu_frequency': host_info.cpu_frequency,
-        'cpu_temperature': host_info.cpu_temp,
-        'current_date': current_date,
-        'disk_io_read_bytes': host_info.disk_io_read_bytes,
-        'disk_io_write_bytes': host_info.disk_io_write_bytes,
-        'disk_space_available': host_info.disk_space_available,
-        'disk_space_used': host_info.disk_space_used,
-        'disk_space_total': host_info.disk_space_total,
-        'disk_partitions': host_info.disk_partitions,
-        'net_io_bytes_recv': host_info.net_io_bytes_recv,
-        'net_io_bytes_sent': host_info.net_io_bytes_sent,
-        'ram': host_info.ram_used,
-        'swap_used': host_info.swap_used,
-        'swap_total': host_info.swap_total,
-    }
-
+    data = reports['usage']()
     print(json.dumps(data))
